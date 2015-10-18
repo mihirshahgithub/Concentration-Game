a3689b8b294080889a04e13ca43e22ca07ae20b0
mihirshahgithub <mshah1086@gmail.com>
2015-10-18 00:34:37 -0700

    Memory_Match V1.0 - Mihir Shah

--- memory match 1.0/mmatch.js
+++ memory match 1.0/mmatch.js
@@ -17,18 +17,21 @@ function card_clicked(card_clicked){
     } else{
         second_card_clicked=$(card_clicked).prev().find('img').attr('src');
         attempts+=1;
-        console.log(attempts);
+        $("#attemptnumber").text(attempts);
 
 
         if(first_card_clicked===second_card_clicked){
             matches+=1;
             accuracy=matches/attempts;
-            console.log(accuracy);
-            alert("You have " + matches + " matches");
+            $("#accuracynumber").text(accuracy * 100 + "%");
+            console.log("You have " + matches + " matches");
             /* Flipping Card Element Back to Original State After Incorrect Match */
         } else{
             $(".back").show(200);
-            matches=null;
+            accuracy=matches/attempts;
+            $("#accuracynumber").text(accuracy *100 + "%");
+
+
 
 
         }
@@ -39,9 +42,33 @@ function card_clicked(card_clicked){
 
     if(matches===total_possible_matches){
         alert("All hail the victor!")
+        $(".back").show(200)
     } else{
         var functionalityComplete="Click handler functionality is complete";
         return console.log(functionalityComplete);
     }
 
 }
+
+    function displayStats(){
+        $('#gamenumber').text(games_played + 1);
+        $('#attemptnumber').text(attempts);
+        $('#accuracystats').text(accuracy * 100 + '%');
+
+
+
+        }
+
+    function reset_stats(){
+        $('.back').show(200);
+        games_played+=1;
+        attempts=0;
+        matches=0;
+        accuracy=0;
+        displayStats();
+
+    }
+
+
+
+
