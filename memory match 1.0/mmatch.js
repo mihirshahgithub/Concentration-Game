feea42d3b499bfdb090f9bd7594145ab5ce93231
mihirshahgithub <mshah1086@gmail.com>
2015-10-18 19:40:14 -0700

    Memory_Match v1.0 - Mihir Shah

--- memory match 1.0/mmatch.js
+++ memory match 1.0/mmatch.js
@@ -17,58 +17,57 @@ function card_clicked(card_clicked){
     } else{
         second_card_clicked=$(card_clicked).prev().find('img').attr('src');
         attempts+=1;
-        $("#attemptnumber").text(attempts);
+        $("#attemptnumber").text(attempts).css("text-align","center");
 
 
         if(first_card_clicked===second_card_clicked){
             matches+=1;
             accuracy=matches/attempts;
-            $("#accuracynumber").text(accuracy * 100 + "%");
+            $("#accuracynumber").text(accuracy * 100 + "%").css("text-align","center");
             console.log("You have " + matches + " matches");
+
             /* Flipping Card Element Back to Original State After Incorrect Match */
-        } else{
+        } else {
+            //matches=null (I need to set matches to null in order for my matches to reset after an incorrect answer, but that causes problems with my accuracy
             $(".back").show(200);
             accuracy=matches/attempts;
-            $("#accuracynumber").text(accuracy *100 + "%");
-
-
-
+            $("#accuracynumber").text(accuracy * 100 + "%").css("text-align","center");
 
         }
-        first_card_clicked=null;
-        second_card_clicked=null;
+            first_card_clicked = null;
+            second_card_clicked = null;
+
 
     }
 
     if(matches===total_possible_matches){
-        alert("All hail the victor!")
+        games_played+=1;
+        matches=null;
+        attempts=null;
+
+        displayStats();
+        alert("All hail the victor!");
         $(".back").show(200)
-    } else{
-        var functionalityComplete="Click handler functionality is complete";
-        return console.log(functionalityComplete);
     }
 
 }
 
-    function displayStats(){
-        $('#gamenumber').text(games_played + 1);
-        $('#attemptnumber').text(attempts);
-        $('#accuracystats').text(accuracy * 100 + '%');
+function displayStats(){
+    accuracy=matches/attempts;
+    $('#gamenumber').text(games_played).css("text-align","center");
+    $('#attemptnumber').text(attempts);
+    $('#accuracystats').text(accuracy * 100 + '%');
 
 
 
-        }
+}
 
-    function reset_stats(){
-        $('.back').show(200);
-        games_played+=1;
-        attempts=0;
-        matches=0;
-        accuracy=0;
-        displayStats();
+function reset_stats(){
+    $('.back').show(200);
+    games_played+=1;
+    attempts=0;
+    matches=0;
+    accuracy=0;
+    displayStats();
 
-    }
-
-
-
-
+}
